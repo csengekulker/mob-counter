1bf703cb1937a822b30954a7c9e53da480552c6f
csengekulker <k71608883429@ktch.hu>
2023-02-27 10:36:29 +0100

    counter + reset works

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, TouchableHighlight, Alert } from 'react-native';
 
 export default function App() {
+  let [ countnum, setCountNum ] = useState(0);
+
+  function increment() {
+    let actualCount = countnum
+    let newCount = ++actualCount
+
+    setCountNum(newCount)
+    console.log(newCount);
+  }
+
+  function resetCounter() {
+    setCountNum(0)
+
+  }
+
+  function showCredits() {
+    // web only
+    alert('Névjegy Balogh Csenge SzoftIIN')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Reset' onPress={resetCounter}>Reset Counter</Button>
+
+      <Text
+        style={styles.title}
+      >Press button to count o_O</Text>
+
+      <Text>{countnum}</Text>
+
+      <TouchableHighlight
+        style={styles.runButton}
+        onPress={increment}
+      >
+        <Text>Count</Text>
+      </TouchableHighlight>
+
+      <Button onPress={showCredits}>Credits</Button>
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -17,4 +54,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  runButton: {
+    backgroundColor: 'rgba(57, 0, 129, 0.2)',
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 3,
+    border: 'solid 2px grey',
+    width: '50%',
+    textAlign: 'center'
+  },
+  title: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
